7d2a103995b4a70ec3a549f9130f1d73d9d252b1
Roberto Slawinski <robertoslawinski@gmail.com>
2025-07-02 12:47:34 +0100

    frontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5005", 
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5005",
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
